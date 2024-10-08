ebd98707dcb3800e236333de53392b698d925842
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2024-10-08 18:27:17 +0700

    push

---
+++ array.php
@@ -0,0 +1,19 @@
+<?php
+$ais = array(
+    "cantik", //0
+    "baik", //1
+    "tidak sombong", //2
+    "suka", //3
+);
+
+$angka = array(
+    1,
+    2,
+    3
+);
+?>
+
+<!-- UNTUK MENAMPILKAN -->
+<?php
+echo $angka[0] . " " . $ais[3] . " " . $ais[2];
+?>
---
+++ assosiate.php
@@ -0,0 +1,17 @@
+<?php
+$keluarga = [
+    "ayah" => "kasmito",
+    "ibu" => "dina",
+    "kakak" => "ais",
+    "adik" => "kaka",
+    "adikkecil" => "fafa",
+];
+$kuliner = [
+    "masakan" => "nasi goreng",
+    "negara" => "indonesia",
+    "kota" => "jakarta",
+];
+
+echo $keluarga["kakak"];
+echo " makan ";
+echo $kuliner["negara"];
